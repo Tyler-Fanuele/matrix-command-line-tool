a3b7ef71e697daa66f951ef363408005b0659371
Hayden Dolan <haydendolan@gmail.com>
2022-03-24 13:24:23 -0400

    Created add_row function

--- matrix.hpp
+++ matrix.hpp
@@ -9,14 +9,18 @@ using std::string;
 using std::getline;
 
 class matrix {
-    public:
-        explicit matrix():rows(0), collumns(0) {}
-        explicit matrix(string first_row);
+ public:
+    explicit matrix():rows(0), collumns(0) {}
+    explicit matrix(string first_row);
+    
+    void add_row(string new_row);
 
-    private:
-        vector<int> mat;
-        int rows;
-        int collumns;
+    int get_rows(void) const {return rows;}
+
+ private:
+    vector<int> mat;
+    int rows;
+    int collumns;
 };
 
 
@@ -24,8 +28,25 @@ matrix::matrix(string first_row):rows(1), collumns(0) {
     string parsed;
     stringstream input_ss(first_row);
 
-    while(getline(input_ss, parsed, ',')) {
+    while (getline(input_ss, parsed, ',')) {
         mat.push_back(stoi(parsed));
         collumns ++;
     }
+}
+
+
+void matrix::add_row(string new_row) {
+    string parsed;
+    stringstream input_ss(new_row);
+    int check_collumns = 0;
+
+    while (getline(input_ss, parsed, ',')) {
+        if (check_collumns >= collumns) {
+            throw std::runtime_error("Additional rows must contain the same number of elements as the previous");
+        }
+        mat.push_back(stoi(parsed));
+        check_collumns++;
+    }
+
+    rows++;
 }
